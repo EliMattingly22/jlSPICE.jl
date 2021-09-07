d39d3c25091cce45e9c41866feb2f46ebd69ae84
Eli Mattingly <ematting@mit.edu>
2021-09-07 08:21:06 -0400

    Remove unneeded files, add export line

--- src/jlSPICE.jl
+++ src/jlSPICE.jl
@@ -1,5 +1,20 @@
 module jlSPICE
-using DataFrames
+using DataFrames, Gtk, PyPlot
+
+include("RunAnalysis.jl")
+include("Load_LTSpice_Net.jl")
+include("SPICE2Matrix.jl")
+
+export RunACAnalysis,
+DetermineTempCo,
+SPICE2Matrix,
+getElementCurrents,
+plotACElCurrent,
+ProcessSPICE_DF,
+SPICE_DF2Matrix_ω,
+LTSpiceLoad,
+MakeNumericalVals
+
 # Write your package code here.
 
 end
